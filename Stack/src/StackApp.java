21389f02def52ce309abe03b6c9631fc5f0507a6
Petr <pmerkuryev@gmail.com>
2025-06-09 13:50:24 +0200

    StackOverflowException added

--- Stack/src/StackApp.java
+++ Stack/src/StackApp.java
@@ -1,3 +1,13 @@
+class StackOverflowException extends RuntimeException {
+    public StackOverflowException(String message) {
+        super(message);
+    }
+}
+class StackUnderflowException extends RuntimeException {
+    public StackUnderflowException(String message) {
+        super(message);
+    }
+}
 class StackX {
     private int maxSize;
     private long[] stackArray;
@@ -9,13 +19,22 @@ class StackX {
         top = -1;
     }
     public void push(long j) {
+        if (isFull()) {
+            throw new StackOverflowException("Стек переполнен");
+        }
         top++;
         stackArray[top] = j;
     }
     public long pop() {
+        if (isEmpty()) {
+            throw new StackUnderflowException("Стек пуст");
+        }
         return stackArray[top--];
     }
     public long peek() {
+        if (isEmpty()) {
+            throw new StackUnderflowException("Стек пуст");
+        }
         return stackArray[top];
     }
     public boolean isEmpty() {
@@ -28,12 +47,16 @@ class StackX {
 
 public class StackApp {
     public static void main(String[] args) {
-        final int SIZE = 10;
+        final int SIZE = 3;
         StackX theStack = new StackX(SIZE);  //make new stack
-        theStack.push(20);
-        theStack.push(40);
-        theStack.push(60);
-        theStack.push(80);
+        try {
+            theStack.push(20);
+            theStack.push(40);
+            theStack.push(60);
+            theStack.push(80);  //invoke exception
+        } catch (StackOverflowException e) {
+            System.out.println(e.getMessage());
+        }
 
         while (!theStack.isEmpty()) {
             long v = theStack.pop();
@@ -41,6 +64,18 @@ public class StackApp {
 
         }
         System.out.println();
+
+        try {
+            System.out.println(theStack.pop());
+            System.out.println(theStack.pop());
+            System.out.println(theStack.pop());
+            System.out.println(theStack.pop());
+            System.out.println(theStack.pop());
+        } catch (StackUnderflowException e) {
+            System.out.println(e.getMessage());
+        }
+
+
 
     }
 }
