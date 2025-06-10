9dba11e53ed53c6b52eaccfad37c4da549279868
Petr <pmerkuryev@gmail.com>
2025-06-10 22:16:00 +0200

    class Queue added

--- Queue/src/QueueApp.java
+++ Queue/src/QueueApp.java
@@ -1,5 +1,36 @@
 class Queue {
+    private int maxSize;
+    private long[] queArray;
+    private int front;
+    private int rear;
+    private int nItems;
+
+    public Queue(int s) {
+        this.maxSize = s;
+        queArray = new long[maxSize];
+        front  =  0;
+        rear   = -1;
+        nItems =  0;
+    }
+    public void insert(long j) {
+        if (rear == maxSize-1)
+            rear = -1;
+        queArray[++rear] = j;
+        nItems++;
+    }
+    public long remove() {
+        long temp = queArray[front++];
+        if (front == maxSize)
+            front = 0;
+        nItems--;
+        return temp;
+    }
+    public long peekFront() {
+        return queArray[front];
+    }
 
 }
+
 public class QueueApp {
+
 }
