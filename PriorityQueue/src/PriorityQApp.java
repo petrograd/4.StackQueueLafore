b2be010311ec8b07738549d6d31aff3449a67d5f
Petr <pmerkuryev@gmail.com>
2025-06-12 14:44:14 +0200

    PriorityQueue and PriorityQApp classes  implemented

--- PriorityQueue/src/PriorityQApp.java
+++ PriorityQueue/src/PriorityQApp.java
@@ -1,8 +1,65 @@
+class QueueOverflowException extends RuntimeException {
+    public QueueOverflowException(String message) {
+        super(message);
+    }
+}
+class QueueUnderflowException extends RuntimeException {
+    public QueueUnderflowException(String message) {
+        super(message);
+    }
+}
 class PriorityQ {
+    //array in sorted order, from max at 0 to min at size-1
+    private final int maxSize;
+    private final long[] queArray;
+    private int nItems;
 
+    public PriorityQ(int s) {
+        maxSize = s;
+        queArray = new long[maxSize];
+        nItems = 0;
+    }
+    public void insert(long v) {
+        if (isFull()) throw new QueueOverflowException("the Queue is overflow with the value " + v);
+        int j = nItems - 1;
+        while( j>=0 && queArray[j] < v) {
+            queArray[j+1] = queArray[j];
+            j--;
+        }
+        queArray[j+1] = v;
+        nItems++;
+    }
+    public long remove() {
+        if (isEmpty()) throw new QueueUnderflowException("the Queue is empty");
+        return queArray[--nItems];
+    }
+    public long peekMin() {
+        return queArray[nItems-1];
+    }
+    public boolean isEmpty() {
+        return (nItems == 0);
+    }
+    public boolean isFull() {
+        return (nItems == maxSize);
+    }
 }
 public class PriorityQApp {
     public static void main(String[] args) {
+        PriorityQ thePQ = new PriorityQ(5);
+        try {
+            thePQ.insert(30);
+            thePQ.insert(50);
+            thePQ.insert(10);
+            thePQ.insert(40);
+            thePQ.insert(20);
+            thePQ.insert(100); // check exception
+        }catch (QueueOverflowException e) {
+            System.out.println(e.getMessage());
+        }
+        while (!thePQ.isEmpty()) {
+            System.out.println(thePQ.remove() + " ");
+        }
+        System.out.println();
 
     }
 }
